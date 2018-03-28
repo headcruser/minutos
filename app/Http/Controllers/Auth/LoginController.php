e7927ef3763f0a27d4d1d7352008fa2831edf52c
headcruser <headcruser@gmail.com>
2018-03-28 13:45:44 -0600

    Sistema de autenticación de laravel

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/minuta';
 
     /**
      * Create a new controller instance.
--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/minuta';
 
     /**
      * Create a new controller instance.
