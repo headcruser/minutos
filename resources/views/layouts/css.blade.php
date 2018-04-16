7fc3f7e473290f608f10700762f99abbdbfafcf3
headcruser <headcruser@gmail.com>
2018-04-16 12:33:18 -0500

    Optimizacion de los archivos css y js mendiante webpack

--- resources/views/layouts/css.blade.php
+++ resources/views/layouts/css.blade.php
@@ -1,23 +1,6 @@
- <!-- Bootstrap 3.3.7 -->
-<link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
-<!-- Font Awesome -->
-<link rel="stylesheet" href="{{ asset('css/font-awesome.min.css') }}">
-<!-- Ionicons -->
-<link rel="stylesheet" href="{{ asset('css/ionicons.min.css') }}">
-<!-- DataTables-->
-<link rel="stylesheet" href="{{ asset('css/dataTables.bootstrap.min.css') }}">
-<!-- Theme style -->
-<link rel="stylesheet" href="{{ asset('css/AdminLTE.min.css') }}">
-<!-- Skin Theme. -->
-<link rel="stylesheet" href="{{ asset('css/_all-skins.min.css') }}">
-<!-- Morris chart -->
-<link rel="stylesheet" href="{{ asset('css/morris.css') }}">
-<!-- jvectormap -->
-<link rel="stylesheet" href="{{ asset('css/jquery-jvectormap.css') }}">
-<!-- Date Picker -->
-<link rel="stylesheet" href="{{ asset('css/bootstrap-datepicker.min.css') }}" >
-<!-- bootstrap wysihtml5 - text editor -->
-<link rel="stylesheet" href="{{ asset('css/bootstrap3-wysihtml5.min.css ') }}">
+ {{-- Admin LTE css --}}
+<link rel="stylesheet" href="{{ asset('css/adminLTE.css') }}">
+
 {{--  Base router  --}}
 <base href="{{url('/')}}">
 <!-- Google Font -->
--- webpack.mix.js
+++ webpack.mix.js
@@ -10,3 +10,36 @@ let mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+mix.scripts([
+  'resources/assets/js/jquery.min.js',
+  'resources/assets/js/jquery-ui.min.js',
+  'resources/assets/js/bootstrap.min.js',
+  'resources/assets/js/raphael.min.js',
+  'resources/assets/js/morris.min.js',
+  'resources/assets/js/jquery.sparkline.min.js',
+  'resources/assets/js/jquery-jvectormap-1.2.2.min.js',
+  'resources/assets/js/jquery-jvectormap-world-mill-en',
+  'resources/assets/js/jquery.knob.min.js',
+  'resources/assets/js/moment.min.js',
+  'resources/assets/js/daterangepicker.js',
+  'resources/assets/js/bootstrap-datepicker.min.js',
+  'resources/assets/js/bootstrap3-wysihtml5.all.min.js',
+  'resources/assets/js/jquery.slimscroll.min.js',
+  'resources/assets/js/fastclick.js',
+  'resources/assets/js/adminlte.min.js',
+  'resources/assets/js/dashboard.js',
+  'resources/assets/js/demo.js',
+],'public/js/adminLTE.js')
+
+mix.styles([
+  'resources/assets/css/bootstrap.min.css',
+  'resources/assets/css/font-awesome.min.css',
+  'resources/assets/css/ionicons.min.css',
+  'resources/assets/css/dataTables.bootstrap.min.css',
+  'resources/assets/css/AdminLTE.min.css',
+  'resources/assets/css/_all-skins.min.css',
+  'resources/assets/css/morris.css',
+  'resources/assets/css/jquery-jvectormap.css',
+  'resources/assets/css/daterangepicker.css',
+  'resources/assets/css/bootstrap3-wysihtml5.min.css',
+], 'public/css/adminLTE.css');
